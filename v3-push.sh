14cc5364cc638f58e7ea45ab695d47605273237d
making <makingx@gmail.com>
2016-10-30 16:37:01 +0900

    Release 1.0.0-dev1

--- v3-push.sh
+++ v3-push.sh
@@ -4,5 +4,6 @@ APP=index-import-task
 SERVICE=df-mysql
 
 ./mvnw clean package -DskipTests=true
-cf v3-push $APP -b java_buildpack_offline -p target/index-import-task-0.0.1-SNAPSHOT.jar
+java -uvf target/index-import-task-1.0.0-dev1.jar Procfile
+cf v3-push $APP -b java_buildpack_offline -p target/index-import-task-1.0.0-dev1.jar
 cf v3-bind-service $APP $SERVICE
